61023e6d674e2ea5a2b535d150f03821fd7db825
dvfleet413 <dvfleet413@gmail.com>
2020-06-27 14:55:51 -0400

    working on navbar

--- src/components/UnauthenticatedNavbar.js
+++ src/components/UnauthenticatedNavbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap'
 
 const UnauthenticatedNavbar = (props) => {
     return (
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand>Second Chance Book Club</Navbar.Brand>
+            <LinkContainer to="/">
+                <Navbar.Brand>Second Chance Book Club</Navbar.Brand>
+            </LinkContainer>
+            <Nav className="mr-auto">
+                <LinkContainer to="/books">
+                    <Nav.Link>All Books</Nav.Link>
+                </LinkContainer>
+            </Nav>
         </Navbar>
     )
 }
