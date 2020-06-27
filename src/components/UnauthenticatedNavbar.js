1be6eda22963abfad4609de7e7af638b48906823
dvfleet413 <dvfleet413@gmail.com>
2020-06-27 14:50:28 -0400

    working on navbar

--- src/components/UnauthenticatedNavbar.js
+++ src/components/UnauthenticatedNavbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 
 const UnauthenticatedNavbar = (props) => {
     return (
-        <div>
-
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand>Second Chance Book Club</Navbar.Brand>
+        </Navbar>
     )
 }
 
--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import UnauthenticatedNavbar from '../components/UnauthenticatedNavbar';
 
 const Layout = (props) => {
     return (
         <div>
+            <UnauthenticatedNavbar />
             {props.children}
         </div>
     )
