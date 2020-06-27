b6218b60e33346aa5ef94e01744d147216a4f028
dvfleet413 <dvfleet413@gmail.com>
2020-06-27 14:59:19 -0400

    added links to book detail pages

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BookCard = (props) => {
     return(
         <div className="book-card">
-            <h1>{props.book.title}</h1>
+            <h1><Link to={`/books/${props.book.bookId}`}>{props.book.title}</Link></h1>
             <h2>By {props.book.author}</h2>
             <img src={props.book.imageUrl} alt={`Picture of the cover of ${props.book.title}`} />
         </div>
