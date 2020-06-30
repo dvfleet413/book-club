a3e8871e6ff494cdcd49f835cb8d550270d82bdb
dvfleet413 <dvfleet413@gmail.com>
2020-06-30 01:37:39 -0400

    working on styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import useAuth from '../hooks/useAuth';
+import { Link } from 'react-router-dom';
 
 const Home = (props) => {
     return (
-        <div>
-            "Hello, stranger!"
+        <div className="home">
+            <h3>Welcome to the Second Chance Book Club!</h3>
+            <small>We tried this once before...</small>
+            <p>Each month we select a new book to read. Check out the <Link to='/books/current'>Current Book</Link> to see what we're reading. If you'd like to join us, <Link to="/register">Create an Account</Link> and click the "I'm In!" button below the current book.</p>
+            <p>Thanks for joining us!</p>
         </div>
     )
 }
